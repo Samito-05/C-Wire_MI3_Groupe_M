b0f775433aa6e20210efffdc29d3ffd3ab057bba
julespel <julespel83110@gmail.com>
2024-12-20 12:39:54 +0100

    Update station.c

--- codeC/station.c
+++ codeC/station.c
@@ -1,5 +1,6 @@
 #include "Station.h"
 
+// Fonction pour retourner le maximum entre deux entiers
 int max(int a, int b){
   if(a < b){
     return b;
@@ -8,6 +9,7 @@ int max(int a, int b){
   }
 }
 
+// Fonction pour retourner le minimum entre deux entiers
 int min(int a, int b){
   if(a < b){
     return a;
@@ -16,12 +18,14 @@ int min(int a, int b){
   }
 } 
 
+// Fonction pour créer un nouveau noeud d'arbre AVL
 pTree createTree(long long int capacity_, long long int consumption_, int id_){
   pTree treeNode = malloc(sizeof(AVLtree));
   if (treeNode == NULL) {
     exit(1);
   }
-  
+
+  // Initialisation des champs du noeud
   treeNode->id = id_;
   treeNode->capacity = capacity_;
   treeNode->consumption = consumption_;
@@ -34,11 +38,13 @@ pTree createTree(long long int capacity_, long long int consumption_, int id_){
   return treeNode;
 }
 
+// Rotation simple à gauche pour équilibrer l'arbre AVL.
 pTree rotationLeft(pTree node) {
   pTree pivot = node->right;
   node->right = pivot->left;
   pivot->left = node;
 
+   // Mise à jour des facteurs d'équilibre.
   int eq_node = node->balance;
   int eq_pivot = pivot->balance;
   
@@ -47,12 +53,13 @@ pTree rotationLeft(pTree node) {
     
   return pivot;
 }
-
+// Rotation simple à droite pour équilibrer l'arbre AVL.
 pTree rotationRight(pTree node) {
     pTree pivot = node->left;
     node->left = pivot->right;
     pivot->right = node;
 
+    // Mise à jour des facteurs d'équilibre.
     int eq_node = node->balance;
     int eq_pivot = pivot->balance;
 
@@ -62,16 +69,19 @@ pTree rotationRight(pTree node) {
     return pivot;
   }
 
+// Rotation double à gauche (rotation droite suivie d'une rotation gauche).
 pTree doubleRotationLeft(pTree node) {
   node->right = rotationRight(node->right);
   return rotationLeft(node);
 }
 
+// Rotation double à droite (rotation gauche suivie d'une rotation droite).
 pTree doubleRotationRight(pTree node) {
   node->left = rotationLeft(node->left);
   return rotationRight(node);
 }
 
+// Fonction pour équilibrer un noeud de l'arbre AVL.
 pTree balanceAVL(pTree node) {
   if (node->balance >= 2) {
     if (node->right->balance >= 0) {
@@ -92,6 +102,7 @@ pTree balanceAVL(pTree node) {
   return node;
 }
 
+// Fonction pour insérer un noeud dans l'arbre AVL.
 pTree insert(pTree node, pTree new, int* height) {
   
   if (node == NULL) {
@@ -99,7 +110,7 @@ pTree insert(pTree node, pTree new, int* height) {
     return new;
   }
 
-  
+  // Si l'identifiant du noeud existe déjà, met à jour les données.
   if (node->id == new->id) {
     node->capacity += new->capacity;
     node->consumption += new->consumption;
@@ -107,22 +118,23 @@ pTree insert(pTree node, pTree new, int* height) {
     return node;
   }
 
-  
+  // Insertion à gauche si l'identifiant est plus petit.
   if (new->id < node->id) {
     node->left = insert(node->left, new, height);
     *height = -(*height);  
   }
-  
+
+  // Insertion à droite si l'identifiant est plus grand.
   else if (new->id > node->id) {
     node->right = insert(node->right, new, height);
   }
 
-  
+  // Mise à jour du facteur d'équilibre après insertion
   if (*height != 0) {
     node->balance += *height; 
     node = balanceAVL(node);  
 
-    
+    // Mise à jour de la hauteur après équilibrage.
     if (node->balance == 0) {
       *height = 0;
     } else {
@@ -133,6 +145,7 @@ pTree insert(pTree node, pTree new, int* height) {
   return node;
 }
 
+// Fonction pour parcourir l'arbre AVL en ordre croissant et écrire dans un fichier.
 void traversal(pTree node, FILE *file) {
     if (node == NULL) {
         return;
@@ -143,7 +156,7 @@ void traversal(pTree node, FILE *file) {
     traversal(node->right, file);
 }
 
-
+// Fonction pour libérer toute la mémoire allouée pour l'arbre AVL.
 void freeTree(pTree node){
   if(node == NULL){
     return;
